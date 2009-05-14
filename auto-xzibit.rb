6ebb0a0152dcef9e3f6195220b5adcd3fd173959
Edward Ocampo-Gooding <edward@edwardog.net>
2009-05-14 00:00:02 -0400

    Attempt at programmatic annotation

--- auto-xzibit.rb
+++ auto-xzibit.rb
@@ -2,21 +2,28 @@ require 'rubygems'
 require 'RMagick'
 require 'sinatra'
 
-get '/' do
-  # haml :index
+def annotate_dawg(message)
   dawg = Magick::ImageList.new("yodawg-dolls-700.jpg")
   text = Magick::Draw.new
-  text.annotate(dawg, 0, 0, 0, 60, "Yo Dawg!") {
+  text.annotate(dawg, 0, 0, 0, 60, message) {
     self.gravity = Magick::SouthGravity
     self.pointsize = 48
     self.stroke = 'transparent'
-    self.fill = '#0000A9'
+    self.fill = '#000000'
     self.font_weight = Magick::BoldWeight
   }
   dawg.write('tmp/sup-dawg.jpg')
+end
+
+get '/' do
+  haml :index
   
-  
-  send_file 'tmp/sup-dawg.jpg', :type => 'image/jpeg', :disposition => 'inline'
+  # send_file 'tmp/sup-dawg.jpg', :type => 'image/jpeg', :disposition => 'inline'
+end
+
+post '/' do
+  annotate_dawg(params[:message])
+  redirect '/'
 end
 
 __END__
@@ -26,4 +33,9 @@ __END__
   = yield
 
 @@ index
-%div.title Yo Dawg.
+  %p Yo Dawg.
+  %img{:src => 'tmp/sup-dawg.jpg'}
+
+  %form{:action => '/', :method => 'post'}
+    %input{:type => 'text', :name => 'message', :value => 'Your new message goes here.'}
+    %input{:type => 'submit', :value => 'Boomshakala. Do it.'}
