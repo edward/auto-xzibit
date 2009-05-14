83111ecb1febe293f3cdc80af15af1ba159ecc7c
Edward Ocampo-Gooding <edward@edwardog.net>
2009-05-14 00:27:54 -0400

    First working version

--- auto-xzibit.rb
+++ auto-xzibit.rb
@@ -2,17 +2,18 @@ require 'rubygems'
 require 'RMagick'
 require 'sinatra'
 
-def annotate_dawg(message)
+def annotate_dawg(message, colour = '#000000')
   dawg = Magick::ImageList.new("yodawg-dolls-700.jpg")
   text = Magick::Draw.new
   text.annotate(dawg, 0, 0, 0, 60, message) {
     self.gravity = Magick::SouthGravity
     self.pointsize = 48
     self.stroke = 'transparent'
-    self.fill = '#000000'
+    self.fill = colour
     self.font_weight = Magick::BoldWeight
   }
   dawg.write('tmp/sup-dawg.jpg')
+  
 end
 
 get '/' do
@@ -27,16 +28,21 @@ post '/' do
   redirect '/'
 end
 
+get '/sup-dawg.jpg' do
+  annotate_dawg(params[:message])
+  send_file 'tmp/sup-dawg.jpg', :type => 'image/jpeg', :disposition => 'inline'
+end
+
 __END__
 
 @@ layout
+!!!
 %html
   = yield
 
 @@ index
-%p Yo Dawg.
-%img{:src => 'tmp/sup-dawg.jpg'}
+%img{:src => "sup-dawg.jpg?message=#{params[:message] || 'Sup dawg?'}"}
 
-%form{:action => '/', :method => 'post'}
-  %input{:type => 'text', :name => 'message', :value => 'Your new message goes here.'}
+%form{:action => '/'}
+  %input{:type => 'text', :name => 'message', :value => 'Say whaaa?'}
   %input{:type => 'submit', :value => 'Boomshakala. Do it.'}
