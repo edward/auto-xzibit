acf4f1c2d314c5ec6cb5f7c011c605501ed3e728
Edward Ocampo-Gooding <edward@edwardog.net>
2009-05-13 23:46:36 -0400

    First image annotation attempt

--- auto-xzibit.rb
+++ auto-xzibit.rb
@@ -2,7 +2,20 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  haml :index
+  # haml :index
+  dawg = Magick::ImageList.new("yodawg-dolls-700.jpg")
+  text = Magick::Draw.new
+  text.annotate(dawg, 0, 0, 0, 60, "Yo Dawg!") {
+    self.gravity = Magick::SouthGravity
+    self.pointsize = 48
+    self.stroke = 'transparent'
+    self.fill = '#0000A9'
+    self.font_weight = Magick::BoldWeight
+  }
+  dawg.write('tmp/sup-dawg.jpg')
+  
+  
+  send_file 'tmp/sup-dawg.jpg', :type => 'image/jpeg', :disposition => 'inline'
 end
 
 __END__
